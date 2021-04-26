b3684b9419a7dbbc90cd05dcef62fbee7b337973
Joao Baumotte <jbaumotte@gmail.com>
2021-04-26 08:18:28 -0300

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import geocode from "./utils/geocode.js"
 import forecast from "./utils/forecast.js"
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
@@ -90,6 +91,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000!')
+app.listen(port, () => {
+  console.log('Server is up on port' + port)
 })
